08c757ccec8b233688d7014934cabf9788d1562a
Sergogovich <giba.sergiy@gmail.com>
2017-01-27 14:55:46 +0200

    +1 alias

--- yellowdots.zsh
+++ yellowdots.zsh
@@ -1,10 +1,10 @@
-#Syslem
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+export PATH="$PATH:$HOME/.rvm/bin"
+
+#Aliases
 alias c="clear"
 
 alias q="cd && clear"
 
-
-#asdf version manager
- 
-
+alias rails5="rvm 2.4.0@rails-5.0.1 && clear"
 
