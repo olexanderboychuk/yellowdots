fde0ee4792891a9a03c515c99c6d85ca3b0c2d36
Sergogovich <giba.sergiy@gmail.com>
2017-02-18 23:46:21 +0200

    Add: aliases for rvm

--- yellowdots.zsh
+++ yellowdots.zsh
@@ -3,10 +3,6 @@ alias c="clear"
 
 alias q="cd && clear"
 
-alias rails5="rvm 2.4.0@rails-5.0.1 && clear"
-
-alias gemset="rvm gemset use "
-
 alias pg="sudo -u postgres psql"
 
 alias restart-wifi="sudo service network-manager restart"
@@ -14,6 +10,11 @@ alias restart-wifi="sudo service network-manager restart"
 alias yloadvrc="echo ':so ~/yellowdots/vim/.yellow_vimrc' >> ~/.vimrc"
 
 alias yloadgcf="echo '[include]' >> ~/.gitconfig && echo '  path = ~/yellowdots/git/.yellow_gitconfig' >> ~/.gitconfig"
+
+#RVM
+alias rl="rvm list"
+alias rgu="rvm gemset use"
+alias rgl="rvm gemset list"
 
 
 #TODO
