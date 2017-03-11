6f7c60fab2ec0ac3bbe11d4db557fc6383e03cc0
Sergogovich <giba.sergiy@gmail.com>
2017-03-11 18:24:51 +0200

    add: alias for download heroku tool

--- yellowdots.zsh
+++ yellowdots.zsh
@@ -7,9 +7,11 @@ alias pg="sudo -u postgres psql"
 
 alias restart-wifi="sudo service network-manager restart"
 
-alias yloadvrc="echo ':so ~/yellowdots/vim/.yellow_vimrc' >> ~/.vimrc"
+alias yload_vrc="echo ':so ~/yellowdots/vim/.yellow_vimrc' >> ~/.vimrc"
 
-alias yloadgcf="echo '[include]' >> ~/.gitconfig && echo '  path = ~/yellowdots/git/.yellow_gitconfig' >> ~/.gitconfig"
+alias yload_gcf="echo '[include]' >> ~/.gitconfig && echo '  path = ~/yellowdots/git/.yellow_gitconfig' >> ~/.gitconfig"
+
+alias yload_heroku="wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh"
 
 alias lo="ruby ~/yellowdots/layout/yellow_layout.rb"
 
